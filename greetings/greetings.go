043e7b673fcf93af86f6a6015778a81bc4bf6705
Yuri Dantas <yurivvdantas@gmail.com>
2021-11-16 20:35:13 -0300

    Return a random greeting
    Introductio to slice

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,6 +2,8 @@ package greetings
 
 import (
 	"errors"
+	"math/rand"
+	"time"
 
 	"fmt"
 )
@@ -13,6 +15,20 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
